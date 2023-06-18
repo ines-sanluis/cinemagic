e035563e94c3194e48afb6f4223dc4127a164a12
ines <insanlur@gmail.com>
2023-06-18 21:56:46 +0200

    Initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
@@ -15,11 +17,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const input = req.body.input || '';
+  if (input.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid input",
       }
     });
     return;
@@ -28,10 +30,19 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(input),
       temperature: 0.6,
+      max_tokens: 1000,
+      top_p: 1
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    const movieArray = JSON.parse(completion.data.choices[0].text);
+    const movieDataPromises = movieArray.map(async (movie) => {
+      const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMBD_API_KEY}&query=${movie}`);
+      return response.data.results[0];
+    });
+
+    const movieData = await Promise.all(movieDataPromises);
+    res.status(200).json({ result: movieData });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -48,15 +59,17 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(input) {
+  return `Create a valid JavaScript array of movie titles that best match this search term ordered from most to least relevant.
+    Generate up to 10 titles.
+    If you are unable to answer the question, return a string that starts with Sorry.
+    The response must be a valid array. Do not escape the double quotes in the output
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+    Example:
+    prompt: "movies with brando"
+
+    response: ["The Godfather", "On the Waterfront", "A Streetcar Named Desire", "The Godfather Part II", "Apocalypse Now"]
+    prompt: ${input}
+    response:
+  `;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,92 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { IoSend } from 'react-icons/io5';
+import {IoEllipsisHorizontal} from 'react-icons/io5';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [input, setInput] = useState("");
+  const [assets, setAssets] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ input: input }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
-      setResult(data.result);
-      setAnimalInput("");
+      console.log(data.result);
+      setAssets(data.result);
+      setIsLoading(false);
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+      setIsLoading(false);
     }
   }
 
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>CineMagic</title>
+        <meta name="description" content="Welcome to CineMagic, your magical cinema search experience." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
+        <section className={styles.hero}>
+          <h1 className={styles.title}>⭐ CineMagic 🪄</h1>
+          <h2 className={styles.subtitle}>Where movie discovery meets AI</h2>
+          <p className={styles.description}>
+            Our unique search feature powered by GPT allows you to find films by plot, actor, or even your current mood!
+            Dive into the magic of cinema with CineMagic
+          </p>
+        </section>
+        <section className={styles.search}>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="search"
+              placeholder="Search a movie"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className={styles.searchBar}
+            />
+            <button type="submit" className={styles.searchIcon}>
+              {isLoading ? <IoEllipsisHorizontal /> : <IoSend />}
+            </button>
+          </form>
+        </section>
+        {assets && <section className={styles.movies}>
+          {assets.map((movie, index) => (
+            <div key={movie.id} className={styles.movie}>
+              {movie.poster_path && (
+                <img
+                className={styles.moviePoster}
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={`Poster for ${movie.title}`}
+                />
+              )}
+              <div className={styles.movieDetails}>
+                <h2>{movie.title}</h2>
+                <p>{movie.overview}</p>
+              </div>
+            </div>
+        ))}
+        </section>}
+        <footer className={styles.footer}>
+          <p>© 2023 CineMagic. All rights reserved.</p>
+        </footer>
       </main>
     </div>
   );
